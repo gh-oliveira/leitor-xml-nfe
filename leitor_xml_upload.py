3e5f4e14996b55214104c20d37fc47046c091680
gh-oliveira <goliveira.srp@gmail.com>
2025-09-14 19:37:54 -0300

    Update leitor_xml_upload.py

--- leitor_xml_upload.py
+++ leitor_xml_upload.py
@@ -21,12 +21,19 @@ def infos_xml(arquivo_xml):
         nome_destinatario = dados_nf["dest"]["xNome"].upper()
         cnpj_destinatario = dados_nf["dest"]["CNPJ"]
         uf_destinatario = dados_nf["dest"]["enderDest"]["UF"]
-        vlr_icms_destino = float(dados_nf["total"]["ICMSTot"].get("vICMSUFDest", 0))
+        vlr_nf = dados_nf["total"]["ICMSTot"]["vNF"]
+        vlr_st_nf = dados_nf["total"]["ICMSTot"]["vST"]
+        vlr_ipi = dados_nf["total"]["ICMSTot"]["vIPI"]
+        vlr_frete = dados_nf["total"]["ICMSTot"]["vFrete"]
+        vlr_icms_destino_nf = (dados_nf["total"]["ICMSTot"].get("vICMSUFDest", 0))
+        vlr_fcp_nf = (dados_nf["total"]["ICMSTot"].get("vFCPUFDest", 0))
+        chave_acesso = dados_nf["Id"][3:]
 
         numero_item = item["@nItem"] if "@nItem" in item else item.get("nItem", "")
         descricao_item = item["prod"]["xProd"].upper()
         ncm_item = item["prod"]["NCM"]
         cfop_item = item["prod"]["CFOP"]
+        vlr_total_item = item["prod"]["vProd"]
         
         pis = list(item["imposto"].get("PIS", {}).values())[0] if "PIS" in item["imposto"] else {}
         cst_pis = pis.get("CST", "")
@@ -36,12 +43,17 @@ def infos_xml(arquivo_xml):
         icms = list(item["imposto"]["ICMS"].values())[0]
         origem_cst = icms.get("orig", "")
         cst_item = origem_cst + icms.get("CST", icms.get("CSOSN", ""))
-        bc_icms_item = float(icms.get("vBC", 0))
+        bc_icms_item = (icms.get("vBC", 0))
         alq_icms_item = icms.get("pICMS", 0)
-        vlr_icms_item = float(icms.get("vICMS", 0))
-        vlr_st_item = float(icms.get("vICMSST", 0))
+        vlr_icms_item = (icms.get("vICMS", 0))
+        vlr_st_item = (icms.get("vICMSST", 0))
 
-        vlr_icms_destino_item = float(item["imposto"].get("ICMSUFDest", {}).get("vBCFCPUFDest", 0))
+        bc_icms_destino_item = (item["imposto"].get("ICMSUFDest", {}).get("vBCUFDest", 0))
+        alq_icms_destino_item = item["imposto"].get("ICMSUFDest", {}).get("pICMSUFDest", 0)
+        vlr_icms_destino_item = (item["imposto"].get("ICMSUFDest", {}).get("vICMSUFDest", 0))
+        bc_fcp_item = (item["imposto"].get("ICMSUFDest", {}).get("vBCFCPUFDest", 0))
+        alq_fcp_item = item["imposto"].get("ICMSUFDest", {}).get("pFCPUFDest", 0)
+        vlr_fcp_item = (item["imposto"].get("ICMSUFDest", {}).get("vFCPUFDest", 0))
         inf_comp = dados_nf.get("infAdic", {}).get("infCpl", "")
 
         conteudo_itens.append({
@@ -52,19 +64,31 @@ def infos_xml(arquivo_xml):
             "Destinatário": nome_destinatario,
             "CNPJ Destinatário": cnpj_destinatario,
             "UF Destinatário": uf_destinatario,
-            "Valor ICMS Destino (Total)": vlr_icms_destino,
+            "Valor Total NFe": vlr_nf,
+            "Valor Total ST": vlr_st_nf,
+            "Valor Total IPI": vlr_ipi,
+            "Valor Total Frete": vlr_frete,
+            "Valor Total ICMS Destino": vlr_icms_destino_nf,
+            "Valor Total FCP": vlr_fcp_nf,
             "Item": numero_item,
             "Descrição": descricao_item,
             "NCM": ncm_item,
             "CST": cst_item,
             "CFOP": cfop_item,
+            "Valor Total Item": vlr_total_item,
             "CST PIS": cst_pis,
             "CST COFINS": cst_cofins,
-            "Base ICMS": bc_icms_item,
-            "Alíquota ICMS": alq_icms_item,
-            "Valor ICMS": vlr_icms_item,
+            "BC ICMS Item": bc_icms_item,            
+            "Alíquota ICMS Item": alq_icms_item,
+            "Valor ICMS Item": vlr_icms_item,
+            "BC ICMS Destino Item": bc_icms_destino_item,
+            "Alíquota ICMS Destino Item": alq_icms_destino_item,
             "Valor ICMS Destino Item": vlr_icms_destino_item,
-            "Valor ST": vlr_st_item,
+            "BC FCP Destino Item": bc_fcp_item,
+            "Alíquota FCP Item": alq_fcp_item,
+            "Valor FCP Item": vlr_fcp_item,
+            "Valor ST Item": vlr_st_item,
+            "Chave de Acesso": chave_acesso,
             "Info Complementar": inf_comp
         })
 
@@ -108,3 +132,4 @@ if uploaded_files:
     except Exception as e:
 
         st.error(f"Erro ao processar os arquivos: {e}")
+
